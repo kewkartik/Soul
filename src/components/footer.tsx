70517e0b85016adde0d5f69c16ea958d79d04cfe
KewKartik <108450560+KewKartik@users.noreply.github.com>
2023-10-25 14:29:31 +0530

    update

---
+++ src/components/footer.tsx
@@ -0,0 +1,14 @@
+"use client"
+import * as React from "react"
+
+export default function Footer() {
+  return (
+    <footer className="bg-border rounded-lg align-baseline shadow m-4 dark:bg-gray-800 mt-auto">
+      <div className="w-full mx-auto p-4 flex items-center justify-center">
+        <span className="text-sm text-gray-500 dark:text-gray-400">
+          Made by KewKartik {':)'}
+        </span>
+      </div>
+    </footer>
+  )
+}
